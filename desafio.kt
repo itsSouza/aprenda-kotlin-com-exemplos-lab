4706ba016aef68415fe9cab782688bb5946e1090
Arthur Souza <84996699+itsSouza@users.noreply.github.com>
2023-08-21 15:55:40 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,61 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+class Usuario(val nome: String, var email: String){
+    override fun toString(): String {
+        return "Usuario: '$nome', email: '$email')"
     }
 }
 
+enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+
+class ConteudoEducacional(val nome: String, var duracao: Int  ){
+    override fun toString(): String {
+        return "Materia: '$nome', duracao: $duracao minutos)"
+    }
+}
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel){
+
+
+    val inscritos = mutableListOf<Usuario>()
+
+    fun matricular(usuario: Usuario){
+
+        inscritos.add(usuario)
+    }
+
+    override fun toString(): String {
+        return "Formacao: '$nome', conteudos: $conteudos, nivel: $nivel, inscritos: $inscritos)"
+    }
+
+}
+
+
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val us1 = Usuario("Arthur Souza", "arthurlucio@gmail.com")
+    val us2 = Usuario("Joao Souza", "jaosouza@gmail.com")
+    val us3 = Usuario("Maria Souza", "maria.souza@gmail.com")
+
+    val conteudosAndroid = mutableListOf(ConteudoEducacional("Conhecendo o Kotlin", 120),
+                                              ConteudoEducacional("POO em Kotlin", 120)      )
+
+    val conteudosJava = mutableListOf(ConteudoEducacional("Logica de Programação em Java", 240),
+        ConteudoEducacional("POO em Java", 120), ConteudoEducacional("Generics", 120))
+
+
+    val conteudosGit = mutableListOf(ConteudoEducacional("Conhecendo Git e GitHub", 60),
+        ConteudoEducacional("Baixando Git e Criando conta GitHub", 60), ConteudoEducacional("Versionamento", 120))
+
+    val formacaoAd = Formacao("Formação Android", conteudosAndroid, Nivel.INTERMEDIARIO)
+    val formacaoJava = Formacao("Formação Java", conteudosJava, Nivel.BASICO)
+    val formacaoGit = Formacao("Formação Git", conteudosGit, Nivel.DIFICIL)
+
+
+
+    formacaoAd.matricular(us1)
+    formacaoJava.matricular(us2)
+    formacaoGit.matricular(us3)
+
+    println(formacaoGit)
+
 }
